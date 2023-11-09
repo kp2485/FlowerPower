33476433096ceb34332b34af1e14c9a1f0037a3d
Kyle Peterson <kdpeters@gmail.com>
2023-11-08 21:45:05 -0500

    Initial Commit

---
+++ FlowerPower/FlowerPowerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowerPowerApp.swift
+//  FlowerPower
+//
+//  Created by Kyle Peterson on 11/8/23.
+//
+
+import SwiftUI
+
+@main
+struct FlowerPowerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
